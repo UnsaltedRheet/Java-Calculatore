112a3d685273db7ecb7005248d54381a4b88c4fb
UnsaltedRheet <unsaltedrheet@gmail.com>
2015-03-09 22:52:14 -0500

    Revert "Revert "Added test panels""
    This reverts commit 615500f06be09666b4f0361bdd8a0098f9749892.

--- Calculatore/src/calculatore/Calculatore.java
+++ Calculatore/src/calculatore/Calculatore.java
@@ -26,7 +26,27 @@ public class Calculatore
         JFrame frame = new JFrame ("WINDOW TITLE");
         frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
         
+        // Set up first subpanel
+         JPanel subPanel1 = new JPanel();
+         subPanel1.setPreferredSize (new Dimension(150, 100));
+         subPanel1.setBackground (Color.green);
+         JLabel label1 = new JLabel ("One");
+         subPanel1.add (label1);
+         // Set up second subpanel
+         JPanel subPanel2 = new JPanel();
+         subPanel2.setPreferredSize (new Dimension(150, 100));
+         subPanel2.setBackground (Color.red);
+         JLabel label2 = new JLabel ("Two");
+         subPanel2.add (label2);
         
+        // Set up primary panel
+         JPanel primary = new JPanel();
+         primary.setBackground (Color.blue);
+         primary.add (subPanel1);
+         primary.add (subPanel2);
+         frame.getContentPane().add(primary);
+         frame.pack();
+         frame.setVisible(true);
     }
     
 }
